ec30600494a6775978075c4be9a2662fc51834ed
Xyliaris <Xyliaris@gmail.com>
2018-02-05 14:18:04 +0100

    change path

--- application/views/ticketmanagement/index.php
+++ application/views/ticketmanagement/index.php
@@ -1,5 +1,17 @@
 
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/jquery.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/jcanvas.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/input.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/color.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/core.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/vector2f.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/rectangle.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/status.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/task.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/area.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/js/mticket/system/taskSystem.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>/assets/js/mticket/manager_ticket.js"></script>
 
-<canvas id="canvas" width="600" height="600">
+<canvas id="canvas" width="1000" height="600">
 
 </canvas>
