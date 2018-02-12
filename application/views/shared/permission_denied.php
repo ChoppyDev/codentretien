ac467ff158f26553020399523afaa9a9a2137c99
MrChoppy <nicolas6720@gmail.com>
2018-02-12 14:16:31 +0100

    permission denied

---
+++ application/views/shared/permission_denied.php
@@ -0,0 +1,5 @@
+<h1> Oups... Une erreur est survenue</h1>
+
+<p> Vous n'avez pas la permission d'accéder à cette page...</p>
+<p> Il se peut que vous soyez déconnecté, <a href="<?php echo base_url()?>login">cliquez ici</a> pour vous reconnecter à nouveau</p>
+<p> Si l'erreur persiste, veuillez contacter un administrateur </p>
