d543651faa3640502393f05a3c31ee39713bac5a
Xyliaris <Xyliaris@gmail.com>
2018-02-14 09:55:47 +0100

    up

--- application/views/administration/index.php
+++ application/views/administration/index.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url()?>assets/js/jqgrid/css/jquery-ui.css"/>
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url() ?>assets/js/jquery-ui.css" />
 <script type="text/javascript" src="<?php echo base_url()?>assets/js/jqgrid/js/jquery.min.js" ></script>
-<script type="text/javascript" src="<?php echo base_url()?>assets/js/jqgrid/js/trirand/jquery.jqGrid.min.js" ></script> 
+<script type="text/javascript" src="<?php echo base_url()?>assets/js/jqgrid/js/trirand/jquery.jqGrid.min.js" ></script>
 <script type="text/javascript" src="<?php echo base_url()?>assets/js/jqgrid/js/jquery-ui.min.js" ></script>
 <script type="text/javascript" src="<?php echo base_url() ?>assets/js/dialog_form.js"></script>
 
@@ -23,43 +23,63 @@
 <button id="create-user">Créer un nouvel utilisateur</button>
 <div id="list2"> </div>
 <div id="form_container">
+  <h1 id="form_title">ajouter un utilisateur</h1>
 <p class="validateTips">Tous les champs sont requis</p>
 	<form>
-			<label for="Username">Nom d'utilisteur:</label>
-			<input type="text" name="Username" id="username">
-			<br>
-			<label for="Password">Mot de passe:</label>
-			<input type="password" name="Password" id="password">
-			<br>
-			<label for="Password_confirmation">Confirmation de mot de passe:</label>
-			<input type="password" name="Password_confirmation" id="password_confirmation">
-			<br>
-			<label for="email">Adresse e-mail:</label>
-			<input type="text" name="email" id="email">
-			<br>
-			<label for="datepicker">Date de Naissance(AAAA-MM-JJ): </label>
-			<input type="text" id="datepicker">
-			<br>
-			<label for="gender">Sexe:</label>
-			<br>
-			<input name="gender" type="radio" id="gender1" value="1"> Masculin	<br>
-	  		<input  name="gender" type="radio" id="gender2" value="2"> Féminin	<br>
-	  		<input  name="gender" type="radio" id="gender3" value="3"> Autre
-	  		<br>
-	  		<label for="numberPhone">Numéro de Téléphone:</label>
-	  		<input type="text" name="numberPhone" id="numberPhone">
-	  		<br>
-	  		<label for="firstName">Prénom:</label>
-	  		<input type="firsName" name="numberPhone" id="firstName">
-	  		<br>
-	  		<label for="lastName">Nom:</label>
-	  		<input type="text" name="lastName" id="lastName">
-	  		<br>
-	  		<label for="groupList">Groupe:</label>
-	  		<select id="groupList">
-	  		<?php for($i = 0; $i < count($this->groups); $i++){ ?>
-	  				<option value="<?php echo $this->groups[$i]->idGroup?>"><?php echo $this->groups[$i]->labelGroup ?> </option>
-	  		<?php } ?>
-	  		</select>
+      <div class="form_bigitem_left">
+        <div class="form_item_left">
+  	      <label for="Username">Nom d'utilisteur</label>
+  	      <input class="form_text_normal_style" type="text" name="Username" id="username">
+        </div>
+          <div class="form_item_left">
+    			<label for="Password">Mot de passe</label>
+    			<input class="form_text_normal_style" type="password" name="Password" id="password">
+        </div>
+        <div class="form_item_left">
+    			<label for="Password_confirmation">Confirmation de mot de passe</label>
+    			<input class="form_text_normal_style" type="password" name="Password_confirmation" id="password_confirmation">
+        </div>
+        <div class="form_item_left">
+    			<label for="email">Adresse e-mail</label>
+    			<input class="form_text_normal_style" type="text" name="email" id="email">
+        </div>
+      </div>
+      <div class="bar_horizontal"></div>
+      <div class="form_bigitem_left">
+        <div class="form_item_left">
+    			<label for="datepicker">Date de Naissance (AAAA-MM-JJ) </label>
+    			<input class="form_text_normal_style" type="text" id="datepicker">
+        </div>
+        <div class="form_item_left">
+    			<label for="gender">Sexe</label>
+    			<br>
+    			  <input name="gender" type="radio" id="gender1" value="1"> <?php for($i=0;$i<60;$i++)echo "&nbsp;";?>Masculin	<br>
+    	  		<input  name="gender" type="radio" id="gender2" value="2"> <?php for($i=0;$i<60;$i++)echo "&nbsp;";?>Féminin	<br>
+    	  		<input  name="gender" type="radio" id="gender3" value="3"> <?php for($i=0;$i<60;$i++)echo "&nbsp;";?>Autre
+          </div>
+          <div class="form_item_left">
+    	  		<label for="numberPhone">Numéro de Téléphone</label>
+    	  		<input class="form_text_normal_style" type="text" name="numberPhone" id="numberPhone">
+          </div>
+          <div class="form_item_left">
+    	  		<label for="firstName">Prénom</label>
+    	  		<input class="form_text_normal_style" type="firsName" name="numberPhone" id="firstName">
+          </div>
+          <div class="form_item_left">
+    	  		<label for="lastName">Nom</label>
+    	  		<input class="form_text_normal_style" type="text" name="lastName" id="lastName">
+          </div>
+        </div>
+        <div class="bar_horizontal"></div>
+        <div class="form_bigitem_left">
+        <div class="form_item_left">
+  	  		<label for="groupList">Groupe</label>
+  	  		<select id="groupList">
+  	  		<?php for($i = 0; $i < count($this->groups); $i++){ ?>
+  	  				<option value="<?php echo $this->groups[$i]->idGroup?>"><?php echo $this->groups[$i]->labelGroup ?> </option>
+  	  		<?php } ?>
+  	  		</select>
+        </div>
+      </div>
 	</form>
 </div>
